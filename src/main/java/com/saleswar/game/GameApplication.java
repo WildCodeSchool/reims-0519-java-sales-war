29898f995f3cde8228430b9feb1cb90fefbd2e63
Youssef Sabouni <50298125+Youssef51@users.noreply.github.com>
2019-07-11 09:38:44 +0200

    clening code

--- src/main/java/com/saleswar/game/GameApplication.java
+++ src/main/java/com/saleswar/game/GameApplication.java
@@ -2,16 +2,14 @@ package com.saleswar.game;
 
 import com.saleswar.game.entities.Opponent;
 
-//import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GameApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(GameApplication.class, args);
-		Opponent firstEnnemy = new Opponent("Germaine the angry grandmother", (Math.random()*50), 75, 250);
-		System.out.println(firstEnnemy.getName()+" | "+firstEnnemy.getLightAttack()+" | "+firstEnnemy.getHeavyAttack()+" | "+firstEnnemy.getLife());
+		SpringApplication.run(GameApplication.class, args);
 	}
 
 }
