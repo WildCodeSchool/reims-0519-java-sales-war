ed0867645b47ffe903215c0b0d894c732a020643
Furkan Celik <c.furkan.contact@gmail.com>
2019-07-11 11:07:44 +0200

    added session name and model name

--- src/main/java/com/saleswar/game/controllers/GameController.java
+++ src/main/java/com/saleswar/game/controllers/GameController.java
@@ -1,8 +1,12 @@
 package com.saleswar.game.controllers;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -23,9 +27,22 @@ class GameController {
         return "scores";
     }
 
-    @PostMapping("/game")
-    public String game() {
+    @PostMapping("/game") 
+        public String game(HttpSession session, Model model, @RequestParam(required=false) String nickname1,@RequestParam(required=false) String nickname2, @RequestParam(required=false) String action) {
+            if(session.getAttribute("nickname1") == null) {
+                // first time we get here : check nickname
+                if(nickname1 == null || nickname1.equals("")) {
+                    // invalid nickname : go back to home page
+                    return "redirect:/";
+                }
+                // register nickname in the session
+                session.setAttribute("nickname1", nickname1);
+                session.setAttribute("nickname2", nickname2);
+                model.addAttribute("nickname1", nickname1);
+                model.addAttribute("nickname2", nickname2);
+            }
         return "game";
+        
     }
 
 }
